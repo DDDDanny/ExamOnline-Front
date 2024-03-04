783956e4850c5d3df577bac3d4adc28e5a171379
DDDDanny <707956456@qq.com>
2024-03-04 15:40:22 +0800

    feat：初始化main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,16 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import router from "./router/index.ts";
 
-createApp(App).mount('#app')
+// 全局Message
+import 'element-plus/es/components/message/style/css'
+import 'element-plus/es/components/message-box/style/css'
+import { ElMessage, ElMessageBox } from "element-plus";
+
+const app = createApp(App)
+
+app.use(router)
+app.use(ElMessage)
+app.use(ElMessageBox)
+app.mount('#app')
