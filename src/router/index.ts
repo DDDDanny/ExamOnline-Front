da01370e443ba64b4264374c358450cae12ebc64
DDDDanny <707956456@qq.com>
2024-03-04 15:40:10 +0800

    feat：初始化Router

---
+++ src/router/index.ts
@@ -0,0 +1,10 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const routers: any = []
+
+export const router = createRouter({
+  history: createWebHistory(),
+  routes: routers,
+})
+
+export default router
