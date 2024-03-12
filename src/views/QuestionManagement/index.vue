cf2f9655c211dcf1b55f3054e0ca161f23452d39
DDDDanny <707956456@qq.com>
2024-03-12 21:54:11 +0800

    feat：新增查询区域查询条件

--- src/views/QuestionManagement/index.vue
+++ src/views/QuestionManagement/index.vue
@@ -4,7 +4,32 @@
       <el-icon><BookMarked /></el-icon>
       <span style="margin-left: 10px">试题管理</span>
     </div>
-    <div class="common-module-query-box"></div>
+    <div class="common-module-query-box">
+      <div class="module-query-item">
+        <span class="module-query-item-title">试题标题: </span>
+        <el-input v-model="queryInfo.topic" placeholder="请输入试题标题" style="width: 220px" clearable />
+      </div>
+      <div class="module-query-item">
+        <span class="module-query-item-title">试题类型: </span>
+        <el-select v-model="queryInfo.type" placeholder="请选择题目类型" style="width: 240px" clearable>
+          <el-option key="1" label="选择题" value="Select" />
+          <el-option key="2" label="判断题" value="Judge" />
+        </el-select>
+      </div>
+      <div class="module-query-item">
+        <span class="module-query-item-title">试题状态: </span>
+        <el-select v-model="queryInfo.status" placeholder="请选择题目状态" style="width: 240px" clearable>
+          <el-option key="1" label="有效" :value="1" />
+          <el-option key="2" label="无效" :value="0" />
+        </el-select>
+      </div>
+      <div class="module-query-item-btn">
+        <el-button type="primary">
+          <Search class="common-btn-icon-style"/>
+          查 询
+        </el-button>
+      </div>
+    </div>
     <div class="common-module-opts-box">
       <el-button type="primary">
         <Plus class="common-btn-icon-style"/>
@@ -45,7 +70,14 @@
 </template>
 
 <script setup lang="ts">
-import { BookMarked, Plus, Trash2 } from "lucide-vue-next";
+import { reactive } from 'vue';
+import { BookMarked, Plus, Trash2, Search } from "lucide-vue-next";
+
+const queryInfo = reactive({
+  topic: '',
+  type: '',
+  status: '',
+})
 
 const tableData = [
   {
