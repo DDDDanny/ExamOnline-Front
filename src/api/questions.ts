8c4c265654263650360f616aab480d2ae2ce7a47
DDDDanny <707956456@qq.com>
2024-03-17 23:20:34 +0800

    feat：新增删除试题接口，并且优化接口方法名

--- src/api/questions.ts
+++ src/api/questions.ts
@@ -2,10 +2,17 @@ import axios from '../utils/request'
 
 
 export class Question {
-  async getQuestions(querySet: object, currentPage: number, pageSize:number) {
+  // 查询获取试题列表
+  async getQuestionsApi(querySet: object, currentPage: number, pageSize:number) {
     const { data } = await axios.get('/question', {
       params: { ...querySet, currentPage, pageSize }
     })
+    return data
+  }
+
+  // 删除试题接口
+  async delQuestionApi(questionId: string) {
+    const { data } = await axios.delete('/question/' + questionId)
     return data
   }
 }
