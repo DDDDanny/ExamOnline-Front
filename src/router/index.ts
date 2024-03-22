5e4524c83b63e148be2db89f2e3fa1791385fc60
DDDDanny <707956456@qq.com>
2024-03-22 11:24:27 +0800

    feat：增加路由守卫，确保登录态后跳转页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import {getCookie} from "../utils/cookie.ts";
 import Login from '../views/Login/index.vue'
 import Container from '../components/Container.vue'
 import HomePage from  '../views/HomePage/index.vue'
@@ -35,5 +36,19 @@ export const router = createRouter({
   history: createWebHistory(),
   routes: routers,
 })
+
+// 增加路由守卫，确保登录态后跳转页面
+// 使用 router.beforeEach 注册一个全局前置守卫，对所有路由导航进行拦截
+router.beforeEach((to, _, next) => {
+  // 用户必须登录才能访问任何页面
+  const isAuthenticated = localStorage.getItem('TOKEN');
+  const userInfo = getCookie('UserInfo')
+  if (to.path !== '/login' && (!isAuthenticated || !userInfo)) {
+    next('/login'); // 如果用户未登录，将其重定向到登录页面
+  } else {
+    next(); // 如果用户已登录，继续路由导航
+  }
+});
+
 
 export default router
