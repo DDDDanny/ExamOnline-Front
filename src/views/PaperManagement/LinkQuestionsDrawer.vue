46c8888acd2ab44c50e9e09d5e14cba72af72b9e
DDDDanny <707956456@qq.com>
2024-03-27 17:32:26 +0800

    feat：新增关联试题的Drawer组件

---
+++ src/views/PaperManagement/LinkQuestionsDrawer.vue
@@ -0,0 +1,36 @@
+<template>
+  <el-drawer
+      :size="800"
+      title="关联试题"
+      v-model="props.drawerVisible"
+      destroy-on-close
+      :close-on-click-modal="false"
+      :close-on-press-escape="false"
+  >
+    <template #footer>
+      <div style="flex: auto">
+        <el-button @click="handleClose">取消</el-button>
+        <el-button type="primary" @click="handleClose">提交</el-button>
+      </div>
+    </template>
+  </el-drawer>
+</template>
+
+<script setup lang="ts">
+
+const props = defineProps({
+  drawerVisible: Boolean,
+  paperInfo: Object,
+})
+
+const emits = defineEmits(['close'])
+
+// 处理关闭Dialog
+const handleClose = () => {
+  emits('close')
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
