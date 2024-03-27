105bd4b58dd13f5b4134afb82a452ccf59e3f474
DDDDanny <707956456@qq.com>
2024-03-27 13:00:49 +0800

    feat：新增删除试卷接口

--- src/api/paper.ts
+++ src/api/paper.ts
@@ -3,7 +3,7 @@ import {Result} from "./response.interface.ts";
 
 
 export class Paper {
-  // 查询获取试卷列表
+  // 查询获取试卷列表接口
   async getPapersApi(querySet: object, currentPage: number, pageSize:number): Promise<Result> {
     const { data } = await axios.get('/paper', {
       params: { ...querySet, currentPage, pageSize }
@@ -11,21 +11,27 @@ export class Paper {
     return data
   }
 
-  // 发布试卷
+  // 发布试卷接口
   async paperPublishApi(id: string): Promise<Result> {
     const { data } = await axios.post('/paperPublish', { id })
     return data
   }
 
-  // 取消发布试卷
+  // 取消发布试卷接口
   async paperCancelPublishApi(id: string): Promise<Result> {
     const { data } = await axios.delete('/paperPublish', { data: { id } })
     return data
   }
 
-  // 新增试卷
+  // 新增试卷接口
   async createPaperApi(paperInfo: object): Promise<Result> {
     const { data } = await axios.post('/paper', { ...paperInfo })
+    return data
+  }
+
+  // 删除试卷接口
+  async delPaperApi(paperId: string): Promise<Result> {
+    const { data } = await axios.delete('/paper/' + paperId)
     return data
   }
 }
