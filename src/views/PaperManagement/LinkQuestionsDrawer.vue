5cdd5faf436e7006cdc0092d0bbae082b99a63da
DDDDanny <707956456@qq.com>
2024-03-28 16:42:38 +0800

    feat：新增试卷模块新增、变更的基础UI以及基础交互

--- src/views/PaperManagement/LinkQuestionsDrawer.vue
+++ src/views/PaperManagement/LinkQuestionsDrawer.vue
@@ -9,7 +9,9 @@
     <div class="link-question-drawer-main">
       <el-divider content-position="left" style="margin: 0 0 20px 0">
         <div class="divider-box">
-          <el-icon><Smile /></el-icon>
+          <el-icon>
+            <Smile/>
+          </el-icon>
           <span style="margin-left: 10px">基础信息</span>
         </div>
       </el-divider>
@@ -25,27 +27,47 @@
       </div>
       <el-divider content-position="left" style="margin: 20px 0 20px 0">
         <div class="divider-box">
-          <el-icon><Package /></el-icon>
+          <el-icon>
+            <Package/>
+          </el-icon>
           <span style="margin-left: 10px">模块信息</span>
         </div>
       </el-divider>
       <div class="paper-module-box">
-        <span class="paper-module-empty">暂无模块</span>
-        <el-button type="primary" size="small" style="width: 400px;border-radius: 5px;" >
-          <el-icon ><PackagePlus /></el-icon>
-          <span style="font-size: 13px;margin-left: 10px">新 增 模 块</span>
+        <div class="module-card-box">
+          <div class="module-card-item" v-for="item in paperModules">
+            <span class="del-module-icon"><el-icon><X/></el-icon></span>
+            <span style="margin-bottom: 5px">{{ item['title'] }}</span>
+            <span>{{ item['description'] }}</span>
+          </div>
+          <div class="add-module-card" v-if="paperModules.length < 4">
+            <el-icon size="16" style="margin-bottom: 5px">
+              <PackagePlus/>
+            </el-icon>
+            <span>新增模块</span>
+          </div>
+        </div>
+        <el-button v-if="paperModules.length !== 0" type="primary" size="small" class="change-module-btn">
+          <el-icon>
+            <Combine/>
+          </el-icon>
+          <span style="font-size: 13px;margin-left: 10px">变 更 模 块</span>
         </el-button>
       </div>
       <el-divider content-position="left" style="margin: 20px 0 20px 0">
         <div class="divider-box">
-          <el-icon><Link /></el-icon>
+          <el-icon>
+            <Link/>
+          </el-icon>
           <span style="margin-left: 10px">关联试题</span>
         </div>
       </el-divider>
       <div class="paper-link-box">
         <span class="paper-link-empty">暂无关联试题</span>
-        <el-button type="primary" size="small" style="width: 400px;border-radius: 5px;" >
-          <el-icon ><Link /></el-icon>
+        <el-button type="primary" size="small" style="width: 400px;border-radius: 5px;">
+          <el-icon>
+            <Link/>
+          </el-icon>
           <span style="font-size: 13px;margin-left: 10px">关 联 试 题</span>
         </el-button>
       </div>
@@ -60,12 +82,12 @@
 </template>
 
 <script setup lang="ts">
-import { storeToRefs } from 'pinia'
+import {storeToRefs} from 'pinia'
 import {onBeforeUpdate, ref, watch} from 'vue'
-import { Paper } from '../../api';
+import {Paper} from '../../api';
 import {ElMessage} from "element-plus";
-import {Smile, Package, Link, PackagePlus} from "lucide-vue-next";
-import { useLinkQuestionStore } from "../../stores/DrawerCommonStore.ts";
+import {Combine, Link, Package, PackagePlus, Smile, X} from "lucide-vue-next";
+import {useLinkQuestionStore} from "../../stores/DrawerCommonStore.ts";
 
 const props = defineProps({
   paperInfo: {
@@ -76,8 +98,8 @@ const props = defineProps({
 
 // 从Store中获取，控制关联试题Drawer是否显示
 const linkQuestionStore = useLinkQuestionStore()
-const { changeDrawerVisible } = linkQuestionStore
-const { drawerVisible } = storeToRefs(linkQuestionStore)
+const {changeDrawerVisible} = linkQuestionStore
+const {drawerVisible} = storeToRefs(linkQuestionStore)
 
 // 试卷基础信息
 const paperBaseInfo: any = ref([])
@@ -86,10 +108,10 @@ const actual_total = ref(0)
 onBeforeUpdate(() => {
   // 更新试卷基础信息
   paperBaseInfo.value = [
-    { key: '试卷标题：', value: props.paperInfo['title']},
-    { key: '试卷描述：', value: props.paperInfo['description']},
-    { key: '答题建议时长：', value: props.paperInfo['duration_minutes']},
-    { key: '计划总分：', value: props.paperInfo['total_marks']},
+    {key: '试卷标题：', value: props.paperInfo['title']},
+    {key: '试卷描述：', value: props.paperInfo['description']},
+    {key: '答题建议时长：', value: props.paperInfo['duration_minutes']},
+    {key: '计划总分：', value: props.paperInfo['total_marks']},
   ]
   // 初始化实际分数
   actual_total.value = props.paperInfo['actual_total']
@@ -150,10 +172,69 @@ watch(drawerVisible, (newValue) => {
     justify-content: center;
     align-items: center;
 
+    .change-module-btn {
+      width: 400px;
+      border-radius: 5px;
+    }
+
     .paper-module-empty {
       color: #b2b2b2;
       font-size: 13px;
       margin-bottom: 20px;
+    }
+
+    .module-card-box {
+      width: 100%;
+      display: grid;
+      overflow: auto;
+      margin-top: 10px;
+      margin-bottom: 20px;
+      grid-row-gap: 50px;
+      grid-column-gap: 20px;
+      justify-content: space-around;
+      grid-template-columns: repeat(auto-fill, 160px);
+      grid-template-rows: repeat(auto-fill, 60px);
+
+      .add-module-card {
+        width: 160px;
+        height: 56px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        font-size: 13px;
+        border-radius: 8px;
+        border: 2px dashed rgba(34, 149, 255, 0.75);
+        color: rgba(34, 149, 255);
+
+        &:hover {
+          cursor: pointer; // hover后鼠标变手指
+        }
+      }
+
+      .module-card-item {
+        position: relative;
+        width: 160px;
+        height: 60px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        font-size: 13px;
+        color: #fff;
+        border-radius: 8px;
+        background-color: rgba(34, 149, 255, 0.75);
+
+        .del-module-icon {
+          position: absolute;
+          top: 8px;
+          right: 8px;
+
+          &:hover {
+            cursor: pointer; // hover后鼠标变手指
+          }
+        }
+      }
     }
   }
 
