f8d2f1f8d3446fb425245952714e6844ffc46c03
DDDDanny <707956456@qq.com>
2024-04-11 21:54:23 +0800

    feat: 完成待关联题库的数据获取逻辑及交互

--- src/views/PaperManagement/QuestionsWarehouseDrawer.vue
+++ src/views/PaperManagement/QuestionsWarehouseDrawer.vue
@@ -8,10 +8,32 @@
       :close-on-click-modal="false"
   >
     <div class="link-questions-warehouse-main">
-        <span style="font-size: 13px;margin-bottom: 10px">
-          Tips：您正在为「{{ props.module['title'] }}」模块添加试题，勾选试题后提交，进行批量配置
-        </span>
-      <div>Content</div>
+      <span style="font-size: 13px;margin-bottom: 10px">
+        Tips：您正在为「 {{ props.module['title'] }} 」模块添加试题，勾选试题后提交，进行批量配置
+      </span>
+      <el-table
+          border
+          stripe
+          size="small"
+          :data="tableData"
+          show-overflow-tooltip
+          class="common-table-base-style"
+          header-cell-class-name="table-header-row-style"
+          max-height="calc(100vh - 210px)"
+      >
+        <el-table-column type="selection" width="40"/>
+        <el-table-column type="index" align="center" width="50" label="序号"/>
+        <el-table-column prop="type" label="试题类型" align="center" width="80">
+          <template #default="scope">
+            <span v-if="scope['row']['type'] === 'select'">选择题</span>
+            <span v-else>判断题</span>
+          </template>
+        </el-table-column>
+        <el-table-column prop="topic" label="试题标题" />
+        <template #empty>
+          <el-image style="width: 300px;opacity: 0.8" src="src/images/noData.png" fit="cover"/>
+        </template>
+      </el-table>
     </div>
     <template #footer>
       <div style="flex: auto">
@@ -23,8 +45,12 @@
 </template>
 
 <script setup lang="ts">
+import { watch, ref } from "vue";
 import {storeToRefs} from "pinia";
 import {Ban, Link} from "lucide-vue-next";
+import {Questions} from '../../api';
+import {ElMessage} from "element-plus";
+import { getCookie } from "../../utils/cookie.ts";
 import {useQuestionsWarehouseStore} from "../../stores/DrawerCommonStore.ts";
 
 // 从Store中获取，控制关联试题-题库Drawer是否显示
@@ -36,6 +62,36 @@ const props = defineProps({
   module: { type: Object, required: true }
 })
 
+// 获取登录用户ID
+const userId = JSON.parse(getCookie('UserInfo')).userId
+
+// 存储表格数据
+const tableData: any = ref([])
+const getQuestionsWarehouse = () => {
+  Questions.getQuestionsWarehouseForPaper(userId).then(response => {
+    if (response.code !== 200) {
+      ElMessage.error(response.msg)
+      return
+    } else {
+      const tempData = []
+      response.data.map(item => {
+        tempData.push({
+          id: item['id'],
+          topic: item['topic'],
+          type: item['type']
+        })
+      })
+      tableData.value = tempData
+    }
+  })
+}
+
+// 监听drawerVisible，当drawerVisible为true时，获取其他数据
+watch(drawerVisible, (newValue) => {
+  if (newValue) {
+    getQuestionsWarehouse()
+  }
+})
 </script>
 
 
@@ -45,5 +101,9 @@ const props = defineProps({
   display: flex;
   flex-direction: column;
   color: #5e5e5e;
+}
+:deep(.table-header-row-style) {
+  background-color: #3483d1 !important;;
+  color: #ffffff !important;;
 }
 </style>
