b1b60548d1dae31064f40736cd7c9016d62c4c13
DDDDanny <707956456@qq.com>
2024-04-18 14:23:40 +0800

    feat: 注册Exam接口类

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,7 +2,8 @@ import UserLogin from './login.ts'
 import Menu from './menu.ts'
 import Questions from './questions.ts';
 import Paper from './paper.ts'
+import Exam from './exam.ts'
 
 export {
-  UserLogin, Menu, Questions, Paper
+  UserLogin, Menu, Questions, Paper, Exam
 }
