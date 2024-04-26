fc2aa42ee461c0cc836727fb40875bc35e864428
DDDDanny <707956456@qq.com>
2024-04-26 23:35:10 +0800

    feat：初始化学生管理页面

---
+++ src/views/UserManagement/StudentsManagement.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="students-main-box">
+    <div class="common-module-header-box">
+      <el-icon>
+        <GraduationCap />
+      </el-icon>
+      <span style="margin-left: 10px">学生管理</span>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { GraduationCap } from "lucide-vue-next";
+</script>
+
+<style scoped lang="scss">
+.students-main-box {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+</style>
