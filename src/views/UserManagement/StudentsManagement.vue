8b2e00e139531d44233c74daffc85844be424163
DDDDanny <707956456@qq.com>
2024-04-27 21:09:22 +0800

    feat：初始化学生管理页面查询和操作区域

--- src/views/UserManagement/StudentsManagement.vue
+++ src/views/UserManagement/StudentsManagement.vue
@@ -6,11 +6,42 @@
       </el-icon>
       <span style="margin-left: 10px">学生管理</span>
     </div>
+    <div class="common-module-query-box">
+      <div class="module-query-item">
+        <span class="module-query-item-title">学生姓名: </span>
+        <el-input v-model="queryInfo.name" placeholder="请输入学生姓名" style="width: 220px" clearable/>
+      </div>
+      <div class="module-query-item">
+        <span class="module-query-item-title">学号: </span>
+        <el-input v-model="queryInfo.student_id" placeholder="请输入学号" style="width: 220px" clearable/>
+      </div>
+      <div class="module-query-item-btn">
+        <el-button type="primary">
+          <Search class="common-btn-icon-style"/>查 询</el-button>
+      </div>
+    </div>
+    <div class="common-module-opts-box">
+      <el-button color="#42b883" style="color: #fff">
+        <Plus class="common-btn-icon-style"/>
+        新 增
+      </el-button>
+      <el-button color="#42b883" style="color: #fff">
+        <Upload class="common-btn-icon-style"/>
+        批量上传
+      </el-button>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { GraduationCap } from "lucide-vue-next";
+import { reactive } from "vue";
+import {GraduationCap, Plus, Search, Upload} from "lucide-vue-next";
+
+// 查询条件
+const queryInfo = reactive({
+  name: null,
+  student_id: null
+})
 </script>
 
 <style scoped lang="scss">
