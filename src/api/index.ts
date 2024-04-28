6cd3298650220263e0b87cceeb240dbc89734bd1
DDDDanny <707956456@qq.com>
2024-04-28 14:24:27 +0800

    feat：注册UserApi

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,7 +3,8 @@ import Menu from './menu.ts'
 import Questions from './questions.ts';
 import Paper from './paper.ts'
 import Exam from './exam.ts'
+import User from './user.ts'
 
 export {
-  UserLogin, Menu, Questions, Paper, Exam
+  UserLogin, Menu, Questions, Paper, Exam, User
 }
