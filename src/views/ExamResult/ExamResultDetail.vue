73211afc08f0fd0db039dd0ed4220a4538f03f54
DDDDanny <707956456@qq.com>
2024-05-14 17:21:56 +0800

    feat: 初始化考试成绩单页面

---
+++ src/views/ExamResult/ExamResultDetail.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="exam-result-detail-main">
+    <el-page-header class="page-header-wording" @back="goBack" :icon="ChevronLeft">
+      <template #content>
+        <span class="page-header-wording"> xxx 考试成绩单 </span>
+      </template>
+    </el-page-header>
+    <el-divider style="margin: 15px 0"/>
+    <div>123</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import router from "../../router";
+import {ChevronLeft} from "lucide-vue-next";
+
+// 返回至考试结果页面
+const goBack = () => {
+  router.replace('/examResult')
+}
+
+</script>
+
+<style scoped lang="scss">
+.exam-result-detail-main {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+}
+.page-header-wording {
+  color:#5e5e5e;
+  font-size: 16px;
+}
+</style>
