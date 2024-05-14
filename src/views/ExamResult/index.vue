0c038b3e5c5c8f7bd0f975a78a0686d2f554992e
DDDDanny <707956456@qq.com>
2024-05-14 16:26:49 +0800

    feat: 初始化考试结果表格数据

--- src/views/ExamResult/index.vue
+++ src/views/ExamResult/index.vue
@@ -16,17 +16,70 @@
           <Search class="common-btn-icon-style"/>查 询</el-button>
       </div>
     </div>
+    <div class="exam-result-table-box">
+      <el-table
+          border
+          stripe
+          size="small"
+          :data="tableData"
+          show-overflow-tooltip
+          class="common-table-base-style"
+          header-cell-class-name="table-header-row-style"
+      >
+        <el-table-column fixed type="index" align="center" width="60" label="序号"/>
+        <el-table-column fixed prop="title" label="考试标题" align="center" width="250"/>
+        <el-table-column prop="start_datetime" label="考试开始时间" align="center" width="180"/>
+        <el-table-column prop="end_datetime" label="考试结束时间" align="center" width="180"/>
+        <el-table-column :resizable="false"/>
+        <el-table-column fixed="right" label="操 作" align="center" width="260" :resizable="false">
+          <template #default>
+            <el-button link size="small" type="success" :icon="FileBadge">查看成绩单</el-button>
+            <el-divider direction="vertical"/>
+            <el-button link size="small" type="primary" :icon="CloudDownload">下载成绩单</el-button>
+          </template>
+        </el-table-column>
+        <template #empty>
+          <el-image style="width: 300px;opacity: 0.8" src="src/images/noData.png" fit="cover"/>
+        </template>
+      </el-table>
+      <div class="exam-result-main-pagination-box">
+        <el-pagination
+            small
+            background
+            :total="tablePageTotal"
+            class="mt-4"
+            style="margin-top: 20px"
+            :default-page-size="pageSize"
+            layout="total, prev, pager, next"
+            @current-change="handleCurrentChange"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { reactive } from "vue";
-import {FileCheck, Search} from "lucide-vue-next";
+import { reactive, ref } from "vue";
+import {FileCheck, FileBadge, Search, CloudDownload} from "lucide-vue-next";
 
 // 查询条件
 const queryInfo = reactive({
   name: null,
 })
+
+// 存储表格数据
+const tableData: any = ref([{}])
+// 当前页
+const currentPage = ref(1)
+// 每页数量
+const pageSize = ref(50)
+// 数据总数
+const tablePageTotal = ref(0)
+
+// 处理分页时当前页的变更事件
+const handleCurrentChange = (val: number) => {
+  currentPage.value = val
+}
 </script>
 
 <style scoped lang="scss">
@@ -35,5 +88,22 @@ const queryInfo = reactive({
   height: 100%;
   display: flex;
   flex-direction: column;
+}
+
+.exam-result-table-box {
+  display: flex;
+  flex-direction: column;
+  height: calc(100vh - 380px);
+}
+
+:deep(.table-header-row-style) {
+  background-color: #3483d1 !important;;
+  color: #ffffff !important;;
+}
+
+.exam-result-main-pagination-box {
+  width: 100%;
+  display: flex;
+  justify-content: flex-end
 }
 </style>
