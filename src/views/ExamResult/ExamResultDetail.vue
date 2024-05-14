1968a2d5ed68badcbe8df30b704a2a3242181222
DDDDanny <707956456@qq.com>
2024-05-14 17:27:33 +0800

    feat: 查看考试成绩单页面新增查询区域

--- src/views/ExamResult/ExamResultDetail.vue
+++ src/views/ExamResult/ExamResultDetail.vue
@@ -6,18 +6,38 @@
       </template>
     </el-page-header>
     <el-divider style="margin: 15px 0"/>
-    <div>123</div>
+    <div class="common-module-query-box">
+      <div class="module-query-item">
+        <span class="module-query-item-title">学生姓名: </span>
+        <el-input v-model="queryInfo.name" placeholder="请输入学生姓名" style="width: 220px" clearable/>
+      </div>
+      <div class="module-query-item">
+        <span class="module-query-item-title">考试分数: </span>
+        <el-input v-model="queryInfo.result_mark" placeholder="请输入考试分数" style="width: 220px" clearable/>
+      </div>
+      <div class="module-query-item-btn">
+        <el-button type="primary" >
+          <Search class="common-btn-icon-style"/>查 询</el-button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { reactive } from 'vue'
 import router from "../../router";
-import {ChevronLeft} from "lucide-vue-next";
+import {ChevronLeft, Search} from "lucide-vue-next";
 
 // 返回至考试结果页面
 const goBack = () => {
   router.replace('/examResult')
 }
+
+// 查询条件
+const queryInfo = reactive({
+  name: null,
+  result_mark: null
+})
 
 </script>
 
