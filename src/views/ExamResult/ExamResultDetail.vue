5728f95e9eb28e4a9d3600d971e7056dd528eecf
DDDDanny <707956456@qq.com>
2024-05-14 19:10:59 +0800

    feat: 初始化查看考试成绩单的表格

--- src/views/ExamResult/ExamResultDetail.vue
+++ src/views/ExamResult/ExamResultDetail.vue
@@ -2,7 +2,7 @@
   <div class="exam-result-detail-main">
     <el-page-header class="page-header-wording" @back="goBack" :icon="ChevronLeft">
       <template #content>
-        <span class="page-header-wording">「{{ examInfo['title'] }}」-- 考试成绩单 </span>
+        <span class="page-header-wording">{{ examInfo['title'] }} -- 考试成绩单 </span>
       </template>
     </el-page-header>
     <el-divider style="margin: 15px 0"/>
@@ -20,15 +20,54 @@
           <Search class="common-btn-icon-style"/>查 询</el-button>
       </div>
     </div>
+    <div class="exam-result-detail-table-box">
+      <el-table
+          border
+          stripe
+          size="small"
+          :data="tableData"
+          show-overflow-tooltip
+          class="common-table-base-style"
+          header-cell-class-name="table-header-row-style"
+      >
+        <el-table-column fixed type="index" align="center" width="60" label="序号"/>
+        <el-table-column fixed prop="title" label="考试标题" align="center" width="250"/>
+        <el-table-column prop="start_time" label="考试开始时间" align="center" width="180"/>
+        <el-table-column prop="end_time" label="考试结束时间" align="center" width="180"/>
+        <el-table-column :resizable="false"/>
+        <el-table-column fixed="right" label="操 作" align="center" width="260" :resizable="false">
+          <template #default>
+            <el-button link size="small" type="success" :icon="FileBadge">查看成绩单</el-button>
+            <el-divider direction="vertical"/>
+            <el-button link size="small" type="primary" :icon="CloudDownload">下载成绩单</el-button>
+          </template>
+        </el-table-column>
+        <template #empty>
+          <el-image style="width: 300px;opacity: 0.8" src="src/images/noData.png" fit="cover"/>
+        </template>
+      </el-table>
+      <div class="exam-result-detail-main-pagination-box">
+        <el-pagination
+            small
+            background
+            :total="tablePageTotal"
+            class="mt-4"
+            style="margin-top: 20px"
+            :default-page-size="pageSize"
+            layout="total, prev, pager, next"
+            @current-change="handleCurrentChange"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { reactive } from 'vue'
+import { reactive, ref } from 'vue'
+import router from "../../router";
 import {storeToRefs} from 'pinia'
 import { useExamResultDetailStore } from "../../stores/ExamResultDetailStore.ts";
-import router from "../../router";
-import {ChevronLeft, Search} from "lucide-vue-next";
+import {ChevronLeft, CloudDownload, FileBadge, Search} from "lucide-vue-next";
 
 // 从Store中获取考试信息
 const examResultDetail = useExamResultDetailStore()
@@ -45,6 +84,20 @@ const queryInfo = reactive({
   result_mark: null
 })
 
+// 存储表格数据
+const tableData: any = ref([{}])
+// 当前页
+const currentPage = ref(1)
+// 每页数量
+const pageSize = ref(50)
+// 数据总数
+const tablePageTotal = ref(0)
+
+// 处理分页时当前页的变更事件
+const handleCurrentChange = (val: number) => {
+  currentPage.value = val
+}
+
 </script>
 
 <style scoped lang="scss">
@@ -54,8 +107,26 @@ const queryInfo = reactive({
   display: flex;
   flex-direction: column;
 }
+
 .page-header-wording {
   color:#5e5e5e;
-  font-size: 16px;
+  font-size: 14px;
+}
+
+:deep(.table-header-row-style) {
+  background-color: #3483d1 !important;;
+  color: #ffffff !important;;
+}
+
+.exam-result-detail-main-pagination-box {
+  width: 100%;
+  display: flex;
+  justify-content: flex-end
+}
+
+.exam-result-detail-table-box {
+  display: flex;
+  flex-direction: column;
+  height: calc(100vh - 380px);
 }
 </style>
