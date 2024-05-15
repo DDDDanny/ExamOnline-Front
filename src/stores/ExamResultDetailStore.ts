573d3496adbb921d47a639bd977c48274d5cfe92
DDDDanny <707956456@qq.com>
2024-05-15 10:41:53 +0800

    feat: 调整ExamResultDetailStore，增加来源路由

--- src/stores/ExamResultDetailStore.ts
+++ src/stores/ExamResultDetailStore.ts
@@ -5,10 +5,13 @@ import { defineStore } from 'pinia';
 // 考试-成绩单详情
 export const useExamResultDetailStore = defineStore('examResultDetail', () => {
   const examInfo: any = ref(null)
+  // 来源路由
+  const sourceUrl: any = ref('/examResult')
 
-  function getExamInfo(item: any) {
+  function getExamInfo(item: any, url: string) {
     examInfo.value = item
+    sourceUrl.value = url
   }
 
-  return { examInfo, getExamInfo }
+  return { examInfo, sourceUrl, getExamInfo }
 })
