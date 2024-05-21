d03d0963970e5f8cf48436fa4926e3ac395cf0bb
DDDDanny <707956456@qq.com>
2024-05-21 10:11:01 +0800

    feat: 新增创建考试结果接口（新增考试-考生关联关系）

--- src/api/examResult.ts
+++ src/api/examResult.ts
@@ -8,6 +8,12 @@ export class ExamResult {
     const { data } = await axios.get('/examResult', { params: { ...querySet } })
     return data
   }
+
+  // 创建考试结果接口（新增考试-考生关联关系）
+  async createExamResultApi(exam_id: string, student_ids: string[]): Promise<Result> {
+    const { data } = await axios.post('/examResult', { exam_id, student_ids })
+    return data
+  }
 }
 
 export default new ExamResult()
