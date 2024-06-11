f40a60514a88ab5be18994dfac6fbf15ad937afc
DDDDanny <707956456@qq.com>
2024-06-11 14:11:07 +0800

    feat：初始化新增教师信息逻辑及交互

--- src/views/UserManagement/TeachersManagement.vue
+++ src/views/UserManagement/TeachersManagement.vue
@@ -21,13 +21,9 @@
       </div>
     </div>
     <div class="common-module-opts-box">
-      <el-button color="#42b883" style="color: #fff">
+      <el-button color="#42b883" style="color: #fff" @click="handleOpenDialog">
         <Plus class="common-btn-icon-style"/>
         新 增
-      </el-button>
-      <el-button color="#42b883" style="color: #fff">
-        <Upload class="common-btn-icon-style"/>
-        批量上传
       </el-button>
     </div>
     <div class="papers-table-box">
@@ -40,7 +36,6 @@
           class="common-table-base-style"
           header-cell-class-name="table-header-row-style"
       >
-        <el-table-column fixed type="selection" width="40"/>
         <el-table-column fixed type="index" align="center" width="60" label="序号"/>
         <el-table-column fixed prop="teacher_id" label="教师编号" align="center" width="180"/>
         <el-table-column prop="name" label="姓名" align="center" width="180"/>
@@ -117,6 +112,46 @@
         />
       </div>
     </div>
+    <el-dialog
+        width="800"
+        title="新增教师用户"
+        draggable
+        destroy-on-close
+        v-model="dialogVisible"
+        :close-on-click-modal="false"
+        :close-on-press-escape="false"
+        @close="handleClose(formRef)"
+    >
+      <el-form :model="formData" ref="formRef">
+        <el-form-item label="教师编号" :label-width="formLabelWidth" prop="teacher_id" required>
+          <el-input v-model="formData.teacher_id" placeholder="请输入教师编号（唯一）" clearable/>
+        </el-form-item>
+        <el-form-item label="教师姓名" :label-width="formLabelWidth" prop="name" required>
+          <el-input v-model="formData.name" placeholder="请输入教师姓名" clearable/>
+        </el-form-item>
+        <el-form-item label="登录账号" :label-width="formLabelWidth" prop="username">
+          <el-input v-model="formData.username" placeholder="请输入登录账号（若为空，默认教师编号为登录账号）" clearable/>
+        </el-form-item>
+        <el-form-item label="性别" :label-width="formLabelWidth" prop="gender" required>
+          <el-select v-model="formData.gender" placeholder="请选择性别">
+            <el-option label="男" value="male"/>
+            <el-option label="女" value="female"/>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="电话" :label-width="formLabelWidth" prop="phone">
+          <el-input v-model="formData.phone" placeholder="请输入电话" clearable/>
+        </el-form-item>
+        <el-form-item label="邮箱" :label-width="formLabelWidth" prop="email">
+          <el-input v-model="formData.email" placeholder="请输入邮箱" clearable/>
+        </el-form-item>
+      </el-form>
+      <template #footer>
+        <div class="dialog-footer">
+          <el-button @click="dialogVisible = false" :icon="Ban">取 消</el-button>
+          <el-button type="primary" @click="dialogVisible = false" :icon="Send">提 交</el-button>
+        </div>
+      </template>
+    </el-dialog>
   </div>
 </template>
 
@@ -124,10 +159,12 @@
 import {onMounted, reactive, ref} from 'vue'
 import { User } from "../../api"
 import {ElMessage, ElMessageBox} from "element-plus";
-import {
-  Users, Plus, Search, Upload,
-  SquarePen, Trash2, X, Check
-} from "lucide-vue-next";
+import {getCookie} from "../../utils/cookie.ts";
+import type {FormInstance} from 'element-plus'
+import {Users, Plus, Search, SquarePen, Trash2, X, Check, Ban, Send} from "lucide-vue-next";
+
+// 获取UserID
+const userId = JSON.parse(getCookie('UserInfo')).userId
 
 // 查询条件
 const queryInfo = reactive({
@@ -193,6 +230,38 @@ const handleDelete = (rowId: string) => {
   })
 }
 
+// Dialog中Form Label的通用宽度
+const formLabelWidth = '100px'
+// 控制新增&编辑用户信息Dialog
+const dialogVisible = ref(false)
+// 处理打开新增&编辑用户信息Dialog
+const handleOpenDialog = () => {
+  dialogVisible.value = true
+}
+// 新增教师表单的Ref
+const formRef = ref<FormInstance>()
+// FormData 初始化
+const initFormData = {
+  teacher_id: '',
+  name: '',
+  username: '',
+  password: '123456',
+  role: 'teacher',
+  gender: '',
+  phone: '',
+  email: '',
+  is_active: true,
+  created_user: userId,
+  updated_user: userId
+}
+// 试题 FormData
+const formData = ref(initFormData)
+// 处理关闭Dialog回调函数
+const handleClose = (createFormEl: any) => {
+  formData.value = initFormData
+  createFormEl.resetFields()
+  dialogVisible.value = false
+}
 </script>
 
 <style scoped lang="scss">
