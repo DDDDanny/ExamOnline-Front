93a676422b2935053d25565441ad30e058d76c8a
DDDDanny <707956456@qq.com>
2024-06-21 18:31:41 +0800

    feat: 新增Inspect插件

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,12 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import Inspect from 'vite-plugin-inspect'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue(), Inspect(),
     AutoImport({
       resolvers: [ElementPlusResolver()],
     }),
