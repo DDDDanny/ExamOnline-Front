156d8792e6ac070494eafab2c48a373313104d66
DDDDanny <707956456@qq.com>
2024-08-05 18:59:14 +0800

    perf: 调整优化引导页Icon

--- src/views/ExamOnline/ExamOnlineGuidePage.vue
+++ src/views/ExamOnline/ExamOnlineGuidePage.vue
@@ -2,7 +2,7 @@
   <div class="exam-main-box">
     <div class="common-module-header-box">
       <el-icon>
-        <MapPinned />
+        <SwatchBook />
       </el-icon>
       <span style="margin-left: 10px">在线考试引导页</span>
     </div>
@@ -10,7 +10,7 @@
 </template>
 
 <script setup lang="ts">
-import { MapPinned } from "lucide-vue-next";
+import { SwatchBook } from "lucide-vue-next";
 </script>
 
 <style scoped lang="scss">
