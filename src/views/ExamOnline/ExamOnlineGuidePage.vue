a1479944e7fdc893160f07c1eaa9067ac40bdea6
DDDDanny <707956456@qq.com>
2024-08-05 18:45:40 +0800

    feat: 初始化在线考试引导页

---
+++ src/views/ExamOnline/ExamOnlineGuidePage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="exam-main-box">
+    <div class="common-module-header-box">
+      <el-icon>
+        <MapPinned />
+      </el-icon>
+      <span style="margin-left: 10px">在线考试引导页</span>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { MapPinned } from "lucide-vue-next";
+</script>
+
+<style scoped lang="scss">
+
+</style>
