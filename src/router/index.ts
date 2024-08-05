1f054ef78710b1551d6f6ba0fa0eb93b8cbf2421
DDDDanny <707956456@qq.com>
2024-08-05 18:45:50 +0800

    feat: 配置在线考试引导页

--- src/router/index.ts
+++ src/router/index.ts
@@ -14,6 +14,7 @@ import ExamResult from "../views/ExamResult/index.vue"
 import ExamResultDetail from "../views/ExamResult/ExamResultDetail.vue";
 import PaperDetailView from "../views/PaperManagement/PaperDetailView.vue";
 import TeachersManagement from "../views/UserManagement/TeachersManagement.vue";
+import ExamOnlineGuidePage from "../views/ExamOnline/ExamOnlineGuidePage.vue";
 
 const routers: any = [
   {
@@ -72,6 +73,10 @@ const routers: any = [
       {
         path: '/teachersManagement',
         component: TeachersManagement,
+      },
+      {
+        path: '/examOnline',
+        component: ExamOnlineGuidePage,
       },
     ]
   }
