849811efd5d724dba98fce9caa30e6e4bfa0fad5
DDDDanny <707956456@qq.com>
2024-10-14 16:52:43 +0800

    feat：初始化在线考试页面

---
+++ src/views/ExamOnline/ExamOnlinePage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="exam-online-main-box">
+    <div class="common-module-header-box">
+      <el-icon>
+        <MonitorCheck />
+      </el-icon>
+      <span style="margin-left: 10px">在线考试</span>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {MonitorCheck} from "lucide-vue-next";
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
