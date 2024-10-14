eef01d3924f311f7e85f70cf6447f8a580b3bff3
DDDDanny <707956456@qq.com>
2024-10-14 19:10:36 +0800

    feat：新增通过考试ID和学生ID获取考试记录ID接口

--- src/api/examOnline.ts
+++ src/api/examOnline.ts
@@ -10,6 +10,14 @@ export class ExamOnline {
     })
     return data
   }
+
+  // 通过考试ID和学生ID获取考试记录ID接口
+  async getStudentExamApi(examId:string, studentId: string): Promise<Result> {
+    const { data } = await axios.get('/examOnlineGetResult', {
+      params: { examId, studentId }
+    })
+    return data
+  }
 }
 
 export default new ExamOnline()
