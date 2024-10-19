ccf591fb82b83506658c64c87a256a619cec3cb7
DDDDanny <707956456@qq.com>
2024-10-19 23:31:39 +0800

    feat：新增考试结果展示页公共组件

---
+++ src/components/CommonResultView.vue
@@ -0,0 +1,129 @@
+<template>
+  <div class="case-list-main">
+    <div id="paperTitle" class="paper-title-info">
+      <span style="font-size: 25px;font-weight: bolder;">{{ props.paperInfo['title'] }}</span>
+      <span style="font-size: 18px;color: #5e5e5e;margin-top: 15px">{{ props.paperInfo['description'] }}</span>
+    </div>
+    <div v-if="paperModuleQuestion.length === 0" style="display: flex;width:98%;justify-content: center;margin-top: 50px">
+      <el-image style="width: 300px;opacity: 0.8" src="/src/images/noData.png" fit="cover"/>
+    </div>
+    <div v-else class="paper-module-box" v-for="(item) in paperModuleQuestion ">
+      <div class="module-info-box">
+        <span>{{ item['title'] }}（{{ item['description'] }}）</span>
+      </div>
+      <div v-if="item['questions'].length === 0">
+        <el-image style="width: 250px;opacity: 0.8" src="/src/images/noData.png" fit="cover"/>
+      </div>
+      <div v-else class="paper-case-list" v-for="(question, index) in item['questions']">
+        <span>{{ index + 1 }}. {{ question['question_detail']['topic'] }}（ {{ question['marks'] }}分 ）</span>
+        <el-radio-group v-if="question['question_detail']['type'] === 'judge'" style="margin-top: 20px" >
+          <el-radio :value="true">对</el-radio>
+          <el-radio :value="false">错</el-radio>
+        </el-radio-group>
+        <el-radio-group v-else style="margin-top: 20px" >
+          <el-radio v-for="key in Object.keys(question['question_detail']['options'])" :value="key">
+            {{ key }}. {{ question['question_detail']['options'][key] }}
+          </el-radio>
+        </el-radio-group>
+      </div>
+    </div>
+    <div class="end-line-style" v-if="paperModuleQuestion.length !== 0">
+      <span>----- 我是底线 -----</span>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, watch} from "vue";
+import { Paper } from "../api";
+import { ElMessage } from "element-plus";
+
+const props = defineProps({
+  paperInfo: { type: Object, required: true, default: () => ({}) },
+})
+
+const paperModuleQuestion = ref([])
+
+const getExamResultInfo = () => {
+  // 获取试题模块信息
+  Paper.getCompletePaperApi(props.paperInfo.id).then((response: any) => {
+    if (response.code !== 200) {
+      ElMessage.error(response.message)
+      return
+    }
+    // 处理试题选项
+    response.data.forEach((item: any) => {
+      item.questions.forEach((element: any) => {
+        if (element.question_detail.options !== 'T&F') {
+          element.question_detail.options = JSON.parse(element.question_detail.options)
+        }
+      })
+    })
+    paperModuleQuestion.value = response.data
+  })
+}
+
+watch(() => props.paperInfo, (newValue) => {
+  if (newValue && Object.keys(newValue).length > 0) {
+    getExamResultInfo()
+  }
+}, { immediate: true })
+</script>
+
+
+<style scoped lang="scss">
+.case-list-main {
+  width: 100%;
+  height: calc(100vh - 180px);
+  display: flex;
+  flex-direction: column;
+  overflow-y: auto;
+}
+.paper-title-info {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 20px;
+}
+
+.paper-case-list {
+  width: 94%;
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  background: #ffffff;
+  box-shadow: 0 0 8px rgba(154, 154, 154, 0.5);
+  border-radius: 10px;
+  padding: 20px 30px;
+  color: #5e5e5e;
+}
+
+.paper-module-box {
+  width: 98%;
+  margin-top: 30px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .module-info-box {
+    width: 100%;
+    height: 30px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 5px;
+    background: #606060;
+    color: #fff;
+    font-size: 13px;
+  }
+}
+.end-line-style {
+  width: 100%;
+  display: flex;
+  margin-top: 20px;
+  justify-content: center;
+  font-size: 13px;
+  color: #a4a4a4
+}
+</style>
